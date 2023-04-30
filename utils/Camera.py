ae64a9456663e303db0f5ca8ae1de1252a5dc846
laviserva <thundereme@gmail.com>
2023-04-29 20:09:50 -0600

    Camera

--- utils/Camera.py
+++ utils/Camera.py
@@ -1,9 +1,26 @@
 import numpy as np
-from utils.Config import Keys
+from utils.Config import Keys_config
 from pyrr import Vector3, vector, vector3, matrix44
 
 class Camera:
-    def __init__(self, position: list[float] = [0.0, 0.0, 10.0]) -> None:
+    def __init__(self, mouse_sensitivity:float = 0.25, jaw:int = -90,
+                pitch:int = 0, constrain_value:int = 90, velocity:float = 0.05,
+                position: list[float] = [0.0, 0.0, 10.0]) -> None:
+        """
+        Args:
+            mouse_sensitivity (float, optional):
+                Determines how much the camera should move in response to mouse movement. Defaults to 0.25.
+            jaw (int, optional):
+                Represents the horizontal rotation of the camera, Defaults to -90. 
+            pitch (int, optional):
+                Represents the vertical rotation of the camera. Defaults to 0.
+            constrain_value (int, optional):
+                Maximum angle that the camera can rotate vertically. Defaults to 90.
+            velocity (float, optional):
+                How fast the camera should move in response to keyboard input. Defaults to 0.05.
+            position (list[float], optional):
+                Initial position of camera. Defaults to [0.0, 0.0, 10.0].
+        """
         # Where is the camera
         self.camera_pos = Vector3(position)
         self.camera_front = Vector3([0.0, 0.0, -1.0])
@@ -11,11 +28,12 @@ class Camera:
         self.camera_right = Vector3([1.0, 0.0, 0.0])
 
         # Configuration for movility
-        self.mouse_sensitivity = 0.25
-        self.jaw = -90
-        self.pitch = 0
-        self.constrain_value = 90
-        self.velocity = 0.05
+        self.mouse_sensitivity = mouse_sensitivity
+        self.jaw = jaw
+        self.pitch = pitch
+        self.constrain_value = constrain_value
+        self.velocity = velocity
+
 
     def get_view_matrix(self) -> matrix44:
         return matrix44.create_look_at(self.camera_pos, self.camera_pos + self.camera_front, self.camera_up)
@@ -46,17 +64,17 @@ class Camera:
         self.camera_right = vector.normalise(vector3.cross(self.camera_front, Vector3([0.0, 1.0, 0.0])))
         self.camera_up = vector.normalise(vector3.cross(self.camera_right, self.camera_front))
 
-    def process_keyboard(self, direction: Keys) -> None:
+    def process_keyboard(self, direction: Keys_config) -> None:
         """Camera method for the WASD movement"""
-        if direction == Keys.FORWARD:
+        if direction == Keys_config.FORWARD:
             self.camera_pos += self.camera_front * self.velocity
-        if direction == Keys.BACKWARD:
+        if direction == Keys_config.BACKWARD:
             self.camera_pos -= self.camera_front * self.velocity
-        if direction == Keys.LEFT:
+        if direction == Keys_config.LEFT:
             self.camera_pos -= self.camera_right * self.velocity
-        if direction == Keys.RIGHT:
+        if direction == Keys_config.RIGHT:
             self.camera_pos += self.camera_right * self.velocity
-        if direction == Keys.UP:
+        if direction == Keys_config.UP:
             self.camera_pos += self.camera_up * self.velocity
-        if direction == Keys.DOWN:
+        if direction == Keys_config.DOWN:
             self.camera_pos -= self.camera_up * self.velocity
