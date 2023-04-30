74044ff353ad7a6c0bc76e4c50476fd3c3388ba6
laviserva <thundereme@gmail.com>
2023-04-29 21:18:10 -0600

    Keys feature added with nameError

--- utils/RendOs.py
+++ utils/RendOs.py
@@ -8,6 +8,8 @@ from typing import List
 
 import pyrr
 
+from OpenGL.GL import glUniformMatrix4fv, GL_FALSE
+
 class RendOs:
     GUI_ = GUI_config
 
@@ -25,6 +27,11 @@ class RendOs:
         self.target: List = [0, 0, 0]
         self.up: List = [0, 1, 0]
 
+        self.renderer = Renderer(width = self.width, height =self.height, title = self.title, width_initpos = self.width_initpos,
+                            height_initpos=self.height_initpos, fovy = self.fovy, z_near = self.z_near, far = self.far,
+                            eye = self.eye, target = self.target, up = self.up,
+                            )
+
         # Camera Properties
         self.mouse_sensitivity:float = 0.25
         self.jaw:int = -90
@@ -35,12 +42,22 @@ class RendOs:
 
         # Keys
         self.gui = GUI_config.GLFW
+        self.keys_inputs = Keys_input(self.gui)
+        self.renderer.keys_inputs = self.keys_inputs
 
         # Camera
         self.cam = Camera()
 
         # 3D Model
         self.obj_model = None
+
+    def _camera_update_from_keys(self):
+        commands = self.keys_inputs.do_movement()
+        [self.cam.process_keyboard(com) for com in commands if com is not []]
+        
+        self.view = self.cam.get_view_matrix()
+        glUniformMatrix4fv(self.renderer._view_loc, 1, GL_FALSE, self.view)
+
     
     def load_model(self, obj_dir, texture_dir, all_objects = True, initial_position = [0, 0, 0]):
         self.obj_model = ObjLoader(obj_dir, texture_path=texture_dir, all_objects=all_objects)
@@ -48,8 +65,12 @@ class RendOs:
         self.obj_model["Plane"]["init_pos"] = pyrr.matrix44.create_from_translation(pyrr.Vector3(initial_position))
     
     def run(self, function=None):
-        renderer = Renderer(width = self.width, height =self.height, title = self.title, width_initpos = self.width_initpos,
-                            height_initpos=self.height_initpos, fovy = self.fovy, z_near = self.z_near, far = self.far,
-                            eye = self.eye, target = self.target, up = self.up,
-                            )
-        renderer.render(self.obj_model, function=function)
+        functions = self._to_refresh_loop()
+        self.renderer._load_functions(functions)
+        self.renderer.render(self.obj_model, function=function)
+
+    def _to_refresh_loop(self):
+        functions = [
+            self._camera_update_from_keys
+            ]
+        return functions
