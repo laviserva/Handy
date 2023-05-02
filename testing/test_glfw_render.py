5704ac8b354035f16ac91dc7956876dcef76f47b
laviserva <thundereme@gmail.com>
2023-05-02 14:09:08 -0600

    rename render

--- testing/test_glfw_render.py
+++ testing/test_glfw_render.py
@@ -6,7 +6,7 @@ current_dir = os.path.dirname(file_dir)
 parent_dir = os.path.dirname(current_dir)
 sys.path.append(parent_dir)
 
-from utils.GLFW_render import Renderer
+from utils.Render_lib.glfw_render import GLFW_Renderer
 import unittest
 
 class Test_glfw_render(unittest.TestCase):
@@ -16,21 +16,21 @@ class Test_glfw_render(unittest.TestCase):
     
     def test_init(self):
 
-        self.assertRaises(ValueError, Renderer, width="12")
-        self.assertRaises(ValueError, Renderer, width=0)
-        self.assertRaises(ValueError, Renderer, width=-8)
+        self.assertRaises(ValueError, GLFW_Renderer, width="12")
+        self.assertRaises(ValueError, GLFW_Renderer, width=0)
+        self.assertRaises(ValueError, GLFW_Renderer, width=-8)
 
-        self.assertRaises(ValueError, Renderer, height="12")
-        self.assertRaises(ValueError, Renderer, height=0)
-        self.assertRaises(ValueError, Renderer, height=-8)
+        self.assertRaises(ValueError, GLFW_Renderer, height="12")
+        self.assertRaises(ValueError, GLFW_Renderer, height=0)
+        self.assertRaises(ValueError, GLFW_Renderer, height=-8)
 
-        self.assertRaises(ValueError, Renderer, title="12")
-        self.assertRaises(ValueError, Renderer, title="1234567890123456789012345678901") # 31 characters
-        self.assertRaises(ValueError, Renderer, title=0)
-        self.assertRaises(ValueError, Renderer, title=-8)
+        self.assertRaises(ValueError, GLFW_Renderer, title="12")
+        self.assertRaises(ValueError, GLFW_Renderer, title="1234567890123456789012345678901") # 31 characters
+        self.assertRaises(ValueError, GLFW_Renderer, title=0)
+        self.assertRaises(ValueError, GLFW_Renderer, title=-8)
 
-        self.assertRaises(ValueError, Renderer, width_initpos="12")
-        self.assertRaises(ValueError, Renderer, width_initpos=-8)
+        self.assertRaises(ValueError, GLFW_Renderer, width_initpos="12")
+        self.assertRaises(ValueError, GLFW_Renderer, width_initpos=-8)
 
-        self.assertRaises(ValueError, Renderer, height_initpos="12")
-        self.assertRaises(ValueError, Renderer, height_initpos=-8)
+        self.assertRaises(ValueError, GLFW_Renderer, height_initpos="12")
+        self.assertRaises(ValueError, GLFW_Renderer, height_initpos=-8)
